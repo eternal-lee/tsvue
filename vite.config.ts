dea751025e4db967f87596b6b245395729047c43
LiQiang <1176952591@qq.com>
2025-04-03 16:27:10 +0800

    chore: 添加自动引入插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import stylelint from 'vite-plugin-stylelint'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +13,27 @@ export default defineConfig({
   server: {
     host: true
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'chrome > 52'],
+      modernPolyfills: true
+    }),
+    stylelint({
+      fix: true, // 启用自动修复
+      include: ['**/*.{css,scss,sass,less}'], // 指定要检查的文件类型
+      exclude: ['node_modules/**', 'dist/**'], // 排除某些目录或文件
+      configFile: '.stylelintrc.json' // 指定 Stylelint 的配置文件路径
+    }),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router', 'pinia'],
+      resolvers: []
+    }),
+    Components({
+      resolvers: []
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
