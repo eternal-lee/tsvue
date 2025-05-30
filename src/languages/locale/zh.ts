b83bd810799d08dbdd6954a508a3a3b126cf2f30
LiQiang <1176952591@qq.com>
2025-05-30 15:36:52 +0800

    feat: 新增i18n切换

---
+++ src/languages/locale/zh.ts
@@ -0,0 +1,9 @@
+export default {
+  Headers: {
+    name: '中文简体',
+    title: '你做到了！',
+    description:
+      '您已经使用<a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> + <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>成功创建了一个项目。接下来是什么？'
+  },
+  nav: ['主页', '关于']
+}
